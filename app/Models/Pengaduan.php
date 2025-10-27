7b40931e9ef125ac8402ea68aebc05acf341cd74
tristanaulia <Tristanaradennisea@gmail.com>
2025-10-27 15:30:42 +0700

    dashboard sisaw

---
+++ app/Models/Pengaduan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pengaduan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pengaduan';
+
+    protected $fillable = [
+        'nama_pelapor',
+        'terlapor',
+        'kejadian',
+        'deskripsi',
+        'tempat_kejadian',
+        'gambar',
+    ];
+}
