fdccbd14b1c52fc5b01249635d674413b86a7312
glllenn <glennmarcel.xpro@gmail.com>
2025-10-28 10:07:35 +0700

    update welcome

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//dashboard redirct
+    Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
+        $user = Auth::user();
+        if ($user->role === '') {
+            return redirect()->route('');
+        } elseif ($user->role === 'user') {
+            return redirect()->route('siswa.dashboard');
+        }
+        return redirect('/');
+    })->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'role:user'])
+    ->prefix('user')
+    ->name('user.')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('siswa.dashboard');
+                })->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
